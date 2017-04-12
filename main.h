85bef7bd1bab2abae53d0826ffdbfe91e8f74c36
tommygoggles <tommygoggles@gmail.com>
2017-04-12 10:47:43 +0100

    Create main.h

---
+++ main.h
@@ -0,0 +1,26 @@
+
+#include <GL/gl.h>
+    //link library
+    //opengl32
+
+class droorinthing
+{
+    public:
+    void draw();
+    void start();
+    void end();
+    void wirey();
+    void line();
+    void texturator(GLuint* tex, unsigned char r, unsigned char g, unsigned char b);
+    void flipit();
+    void flipfixit();
+       
+    bool fix;
+    bool wire;
+
+    GLuint tex1;
+    GLuint tex2;
+    
+    droorinthing();
+    ~droorinthing();
+};
